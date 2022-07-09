3eea736ffce2e3f6d46760a2ebfd4b3ce51da22e
Laura <lmcsay@gmail.com>
2022-07-09 13:03:42 -0500

    Create new user and add to database

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -2,7 +2,8 @@ const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 
 const register = async (req, res) => {
-  res.status(StatusCodes.CREATED).json(req.body);
+  const user = await User.create({ ...req.body });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 const login = async (req, res) => {
   res.send('login user');
