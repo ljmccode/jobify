8548d99683930a641ea9dce16fed9647ef5de1e2
Laura <lmcsay@gmail.com>
2022-07-09 14:53:33 -0500

    Check if login password is correct and if not throw error

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -7,13 +7,19 @@ const register = async (req, res) => {
   const token = user.createJWT();
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
+
 const login = async (req, res) => {
   const { email, password } = req.body;
+
   if (!email || !password) {
     throw new BadRequestError('Please provide email and password');
   }
   const user = await User.findOne({ email });
   if (!user) {
+    throw new UnauthenticatedError('Invalid Credentials');
+  }
+  const isPasswordCorrect = await user.comparePassword(password);
+  if (!isPasswordCorrect) {
     throw new UnauthenticatedError('Invalid Credentials');
   }
 
