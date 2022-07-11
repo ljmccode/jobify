d00d63719ad4fcc0c4dc88b7115ed00613425bb4
Laura <lmcsay@gmail.com>
2022-07-11 11:43:20 -0500

    Create custom validation error

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -6,13 +6,20 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || 'Something went wrong, try again later',
   };
 
+  if (err.name === 'ValidationError') {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(', ');
+    customError.statusCode = 400;
+  }
+
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
     )} field, please choose another value`;
     customError.statusCode = 400;
   }
-  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  // return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err });
   return res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
