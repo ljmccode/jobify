9228a3e9906c5f9ff46cf2970f1404f5fab1652d
Laura <lmcsay@gmail.com>
2022-07-13 11:03:21 -0500

    Add logo component to Landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,12 +1,12 @@
-import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 import styled from 'styled-components';
+import { Logo } from '../components';
 
 const Landing = () => {
   return (
     <Wrapper>
       <nav>
-        <img src={logo} alt='jobify' className='logo' />
+        <Logo />
       </nav>
       <div className='container page'>
         <div className='info'>
