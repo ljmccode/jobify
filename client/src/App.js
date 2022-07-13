9cfc50e9132b06ae5ceae225b6915624d257164c
Laura <lmcsay@gmail.com>
2022-07-13 17:50:39 -0500

    Setup basic routing structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
-import Landing from "./pages/Landing";
+import Landing from './pages/Landing';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Landing />
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<div>Dashboard</div>}></Route>
+        <Route path='/register' element={<div>Register</div>}></Route>
+        <Route path='/landing' element={<Landing />}></Route>
+        <Route path='*' element={<div>Error</div>}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
