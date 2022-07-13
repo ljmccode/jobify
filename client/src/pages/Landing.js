ec9d93b0aa926e2667f7f1c47b796e6e29bf902e
Laura <lmcsay@gmail.com>
2022-07-12 19:12:02 -0500

    Add elements to the landing page

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -2,8 +2,23 @@ import logo from '../assets/images/logo.svg';
 import main from '../assets/images/main.svg';
 
 const Landing = () => {
-  return <h4>Landing Page</h4>
+  return (
+    <main>
+      <nav>
+        <img src={logo} alt='jobify' className='logo' />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores voluptate blanditiis deserunt nisi quo provident amet recusandae earum, aut optio natus officiis odit eaque architecto accusamus ducimus modi possimus veritatis.</p>
+          <button className='btn btn-hero'>Login/Register</button>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img'/>
+      </div>
+    </main>
+  );
 };
 
 export default Landing;
-
