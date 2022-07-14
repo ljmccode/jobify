7fe2fe53463dd1d9d38ff85471c08b5951115d78
Laura <lmcsay@gmail.com>
2022-07-14 11:38:30 -0500

    Pass in FormRow components to Register page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Logo } from '../components';
+import { Logo, FormRow } from '../components';
 import styled from 'styled-components';
 
 const initialState = {
@@ -26,80 +26,65 @@ const Register = () => {
       <form className='form' onSubmit={onSubmit}>
         <Logo />
         <h3>Login</h3>
-        {/* name field */}
-        <div className='form-row'>
-          <label htmlFor='name' className='form-label'>
-            name
-          </label>
-          <input
-            type='text'
-            value={values.name}
-            name='name'
-            onChange={handleChange}
-            className='form-input'
-          />
-        </div>
-        <div className='form-row'>
-          <label htmlFor='email' className='form-label'>
-            email
-          </label>
-          <input
-            type='email'
-            value={values.email}
-            name='email'
-            onChange={handleChange}
-            className='form-input'
-          />
-        </div>
-        <div className='form-row'>
-          <label htmlFor='password' className='form-label'>
-            password
-          </label>
-          <input
-            type='password'
-            value={values.password}
-            name='password'
-            onChange={handleChange}
-            className='form-input'
-          />
-        </div>
-
+        {/* name input */}
+        <FormRow
+          type='text'
+          name='name'
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* email input */}
+        <FormRow
+          type='email'
+          name='email'
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password input */}
+        <FormRow
+          type='password'
+          name='password'
+          value={values.password}
+          handleChange={handleChange}
+        />
         <button type='submit' className='btn btn-block'>
           Submit
         </button>
-        <p>Not a member yet? <button className='member-btn'>Register</button></p>
+        <p>
+          Not a member yet? <button className='member-btn'>Register</button>
+        </p>
       </form>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.main`
-display: grid;
-align-items: center;
-.logo{
-  display:block;
-  margin: 0 auto;
-  margin-bottom: 1.38rem;
-}
-.form{
-  max-width: 400px;
-  border-top: 5px solid var(--primary-500);
-}
-h3 {
-  text-align: center;
-}
-p {
-  margin: 0;
-  margin-top: 1rem;
-  text-align: center;
-}
-.member-btn {
-  color: var(--primary-500);
-  letter-spacing: var(--letterSpacing);
-  cursor: pointer;
-  background: transparent;
-  border: transparent;
-}
-`
+  display: grid;
+  align-items: center;
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+  .form {
+    max-width: 400px;
+    border-top: 5px solid var(--primary-500);
+  }
+  h3 {
+    text-align: center;
+  }
+  p {
+    margin: 0;
+    margin-top: 1rem;
+    text-align: center;
+  }
+  .member-btn {
+    color: var(--primary-500);
+    letter-spacing: var(--letterSpacing);
+    cursor: pointer;
+    background: transparent;
+    border: transparent;
+  }
+`;
 
 export default Register;
