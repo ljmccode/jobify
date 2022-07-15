467614f2b76bb1bae7463d17e7361861e0403520
Laura <lmcsay@gmail.com>
2022-07-14 19:57:08 -0500

    Throw bad request error if user email already exists

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,12 +3,16 @@ import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, UnauthenticatedError } from '../errors/index.js';
 
 const register = async (req, res) => {
-  const { name, email, password } =req.body;
+  const { name, email, password } = req.body;
 
-  if(!name || !email || !password) {
+  if (!name || !email || !password) {
     throw new BadRequestError('Please provide all values');
   }
 
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    throw new BadRequestError('Email already in use');
+  }
   const user = await User.create({ name, email, password });
   const token = user.createJWT();
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
@@ -34,11 +38,7 @@ const login = async (req, res) => {
 };
 
 const updateUser = (req, res) => {
-  res.send('update user')
-}
-
-export {
-  register,
-  login,
-  updateUser
+  res.send('update user');
 };
+
+export { register, login, updateUser };
