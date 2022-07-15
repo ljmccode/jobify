19f3c0d657f47d7837827cb8eaa5ff9e5cf4bd2a
Laura <lmcsay@gmail.com>
2022-07-15 14:28:28 -0500

    Add login user reducers

--- client/src/context/reducer.js
+++ client/src/context/reducer.js
@@ -4,6 +4,9 @@ import {
   REGISTER_USER_BEGIN,
   REGISTER_USER_SUCCESS,
   REGISTER_USER_ERROR,
+  LOGIN_USER_BEGIN,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_ERROR,
 } from './actions';
 
 const reducer = (state, action) => {
@@ -44,6 +47,35 @@ const reducer = (state, action) => {
     };
   }
   if (action.type === REGISTER_USER_ERROR) {
+    return {
+      ...state,
+      isLoading: false,
+      showAlert: true,
+      alertType: 'danger',
+      alertText: action.payload.msg,
+    };
+  }
+  if (action.type === LOGIN_USER_BEGIN) {
+    return {
+      ...state,
+      isLoading: true
+    };
+  }
+  if (action.type === LOGIN_USER_SUCCESS) {
+    const {token, user, location, jobLocation } = action.payload
+    return {
+      ...state,
+      isLoading: false,
+      token,
+      user,
+      userlocation: location,
+      jobLocation: jobLocation,
+      showAlert: true,
+      alertType: 'success',
+      alertText: 'Login Successful! Redirecting...',
+    };
+  }
+  if (action.type === LOGIN_USER_ERROR) {
     return {
       ...state,
       isLoading: false,
