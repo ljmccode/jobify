f8c5ee99a724a1be68bce24e065f1a63f0e3134a
Laura <lmcsay@gmail.com>
2022-07-14 19:49:43 -0500

    Throw bad request error if user doesn't provide name, email or password

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,7 +3,13 @@ import { StatusCodes } from 'http-status-codes';
 import { BadRequestError, UnauthenticatedError } from '../errors/index.js';
 
 const register = async (req, res) => {
-  const user = await User.create({ ...req.body });
+  const { name, email, password } =req.body;
+
+  if(!name || !email || !password) {
+    throw new BadRequestError('Please provide all values');
+  }
+
+  const user = await User.create({ name, email, password });
   const token = user.createJWT();
   res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
@@ -27,7 +33,12 @@ const login = async (req, res) => {
   res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
+const updateUser = (req, res) => {
+  res.send('update user')
+}
+
 export {
   register,
   login,
+  updateUser
 };
