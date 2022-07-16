d868de575a019bb09cab46f280e8a1c91834fe97
Laura <lmcsay@gmail.com>
2022-07-16 15:21:04 -0500

    Create basic structure to SharedLayout page

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,9 +1,16 @@
+import { Outlet, Link } from 'react-router-dom';
+
+
 const SharedLayout = () => {
   return (
-    <div>
-      Shared Layout Page
-    </div>
-  )
-}
+    <section>
+      <nav>
+        <Link to='add-job'>add job</Link>
+        <Link to='all-jobs'>all job</Link>
+      </nav>
+      <Outlet />
+    </section>
+  );
+};
 
-export default SharedLayout
+export default SharedLayout;
