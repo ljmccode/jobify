8ae1bb21fa83b029da5c3102c6abdf5b9790fc79
Laura <lmcsay@gmail.com>
2022-07-16 22:25:22 -0500

    Add style for dashboard

--- client/src/pages/dashboard/SharedLayout.js
+++ client/src/pages/dashboard/SharedLayout.js
@@ -1,10 +1,10 @@
 import { Outlet } from 'react-router-dom';
 import { Navbar, BigSidebar, SmallSidebar } from '../../components';
+import styled from 'styled-components';
 
 const SharedLayout = () => {
   return (
-    <section>
-      <div>
+    <Wrapper>
         <main className='dashboard'>
           <SmallSidebar />
           <BigSidebar />
@@ -15,9 +15,30 @@ const SharedLayout = () => {
             </div>
           </div>
         </main>
-      </div>
-    </section>
+    </Wrapper>
   );
 };
 
 export default SharedLayout;
+
+const Wrapper = styled.section`
+  .dashboard {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+  
+  .dashboard-page {
+    width: 90vw;
+    margin: 0 auto;
+    padding: 2rem 0;
+  }
+
+  @media (min-width: 992px) {
+    .dashboard {
+      grid-template-columns: auto 1fr;
+    }
+    .dashboard-page {
+      width: 90%;
+    }
+  }
+`
