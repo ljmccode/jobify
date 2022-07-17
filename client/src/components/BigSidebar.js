8b3f66b3c9405c5d1605cb4c266825a4087d640c
Laura <lmcsay@gmail.com>
2022-07-16 22:33:30 -0500

    Use show-sidebar className to toggle big and small sidebars

--- client/src/components/BigSidebar.js
+++ client/src/components/BigSidebar.js
@@ -1,17 +1,51 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { useAppContext } from '../context/appContext';
+import {Logo} from '../components';
 
 const BigSidebar = () => {
+  const { showSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h4>BigSidebar</h4>
+      <div
+        className={
+          showSidebar ? 'sidebar-container ' : 'sidebar-container show-sidebar'
+        }
+      >
+        <div className='content'>
+          <header>
+            <Logo />
+          </header>
+        </div>
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default BigSidebar
+export default BigSidebar;
 
 const Wrapper = styled.aside`
-  @media screen and (max-width: 992px) {
-    display: none;
+  display: none;
+
+  @media screen and (min-width: 992px) {
+    display: block;
+    box-shadow: 1px 0px 0px 0px rgba(0, 0, 0, 0.1);
+
+    .sidebar-container {
+      background: var(--white);
+      min-height: 100vh;
+      height: 100%;
+      width: 250px;
+      margin-left: -250px;
+      transition: var(--transition);
+    }
+
+    .show-sidebar {
+      margin-left: 0;
+    }
+
+    .content{
+      position: sticky;
+      top: 0;
+    }
   }
 `;
--- client/src/components/SmallSidebar.js
+++ client/src/components/SmallSidebar.js
@@ -1,9 +1,17 @@
 import styled from 'styled-components';
+import { useAppContext } from '../context/appContext';
 
 const SmallSidebar = () => {
+  const { showSidebar } = useAppContext();
   return (
     <Wrapper>
-      <h4>SmallSidebar</h4>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >Small Device Sidebar
+        <div className='content'>Content</div>
+      </div>
     </Wrapper>
   );
 };
@@ -11,7 +19,36 @@ const SmallSidebar = () => {
 export default SmallSidebar;
 
 const Wrapper = styled.aside`
-  @media screen and (min-width: 992px) {
+  @media (min-width: 992px) {
     display: none;
+  }
+  
+  .sidebar-container {
+    position: fixed;
+    inset: 0;
+    background: rgba(0, 0, 0, 0.7);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    z-index: -1;
+    opacity: 0;
+    transition: var(--transition);
+  }
+
+  .show-sidebar {
+    z-index: 99;
+    opacity: 1;
+  }
+
+  .content {
+    background: var(--white);
+    width: var(--fluid-width);
+    height: 95vh;
+    border-radius: var(--borderRadius);
+    padding: 4rem 2rem;
+    position: absolute;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
   }
 `;
