1c79fce53abcdeb57a5bda8844cc639ed228c04c
Laura <lmcsay@gmail.com>
2022-07-17 12:25:18 -0500

    Create NavLinks component that displays all navigation links

---
+++ client/src/components/NavLinks.js
@@ -0,0 +1,24 @@
+import links from '../utils/links';
+import { NavLink } from 'react-router-dom';
+
+const NavLinks = () => {
+  return (
+      <div className='nav-links'>
+        {links.map((link) => {
+          const {id, path, text, icon} = link
+          return (
+            <NavLink
+              to={path}
+              key={id}
+              className='nav-link'
+            >
+              <span className='icon'>{icon}</span>
+              {text}
+            </NavLink>
+          )
+        })}
+      </div>    
+  )
+};
+
+export default NavLinks;
