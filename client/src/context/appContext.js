67fc489a50ae2615eedece389df58af7afde9e74
Laura <lmcsay@gmail.com>
2022-07-19 10:34:15 -0500

    Create handle change funciton to dispatch handle change reducer

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -14,6 +14,7 @@ import {
   UPDATE_USER_BEGIN,
   UPDATE_USER_SUCCESS,
   UPDATE_USER_ERROR,
+  HANDLE_CHANGE,
 } from './actions.js';
 
 const token = localStorage.getItem('token');
@@ -147,6 +148,10 @@ const AppProvider = ({ children }) => {
     clearAlert();
   };
 
+  const handleChange = ({name, value}) => {
+    dispatch({ type: HANDLE_CHANGE, payload: { name, value } });
+  };
+
   return (
     <AppContext.Provider
       value={{
@@ -156,6 +161,7 @@ const AppProvider = ({ children }) => {
         toggleSidebar,
         logoutUser,
         updateUser,
+        handleChange,
       }}
     >
       {children}
