3810d964176262aad26827277ffebfeb2a6b35c2
Laura <lmcsay@gmail.com>
2022-07-19 15:01:32 -0500

    Import/Export new components from index.js

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -6,5 +6,9 @@ import Navbar from './Navbar';
 import SmallSidebar from './SmallSidebar';
 import NavLinks from './NavLinks';
 import FormRowSelect from './FormRowSelect';
+import SearchContainer from './SearchContainer';
+import JobsContainer from './JobsContainer';
+import Job from './Job';
+import JobInfo from './JobInfo';
 
-export { Logo, FormRow, Alert, BigSidebar, Navbar, SmallSidebar, NavLinks, FormRowSelect };
+export { Logo, FormRow, Alert, BigSidebar, Navbar, SmallSidebar, NavLinks, FormRowSelect, SearchContainer, JobsContainer, Job, JobInfo };
