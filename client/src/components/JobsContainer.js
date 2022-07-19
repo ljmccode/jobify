ae0b9b7d9cbf530eb52c47216c51c9ee35363d0f
Laura <lmcsay@gmail.com>
2022-07-19 16:14:14 -0500

    Add styling to Job Container component

--- client/src/components/JobsContainer.js
+++ client/src/components/JobsContainer.js
@@ -38,4 +38,24 @@ const JobsContainer = () => {
 
 export default JobsContainer;
 
-const Wrapper = styled.section``;
+const Wrapper = styled.section`
+margin-top: 4rem;
+  h2 {
+    text-transform: none;
+  }
+  & > h5 {
+    font-weight: 700;
+  }
+
+.jobs {
+  display: grid;
+  gap: 2rem;
+}
+
+@media (min-width: 992px) {
+    .jobs {
+      grid-template-columns: 1fr 1fr;
+      gap: 1rem;
+    }
+  }
+`;
