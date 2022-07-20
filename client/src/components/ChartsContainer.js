062ac68b4f52a641b1522a728db12e9d04b278cb
Laura <lmcsay@gmail.com>
2022-07-20 13:51:34 -0500

    Switch between BarChart or AreaChart component with toggle button

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import BarChart from './BarChart';
+import AreaChart from './AreaChart';
+import { useAppContext } from '../context/appContext';
+import styled from 'styled-components';
 
 const ChartsContainer = () => {
-  return <div>ChartsContainer</div>;
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useAppContext();
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+
+      <button type='button' onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+    </Wrapper>
+  );
 };
 
 export default ChartsContainer;
+
+const Wrapper = styled.section``;
