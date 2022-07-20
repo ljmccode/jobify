59321773c8d36a2da59c0539faec539897899384
Laura <lmcsay@gmail.com>
2022-07-19 20:32:12 -0500

    Refactor check permissions

--- server/utils/checkPermissions.js
+++ server/utils/checkPermissions.js
@@ -2,7 +2,9 @@ import { UnauthenticatedError } from '../errors/index.js';
 
 const checkPermissions = (requestUser, resourceUserId) => {
   if (requestUser === 'admin') return;
-  if (requestUser === resourceUserId.toString()) return;
+  if (requestUser === resourceUserId.toString()) {
+    return;
+  } 
   throw new UnauthenticatedError('Not authorized to access this route');
 };
 
