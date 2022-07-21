645d03c2187d0afd5f41680ea439160ce0bffe88
Laura <lmcsay@gmail.com>
2022-07-20 22:03:54 -0500

    Add prev and next page button functionality

--- client/src/components/PageBtnContainer.js
+++ client/src/components/PageBtnContainer.js
@@ -10,10 +10,18 @@ const PageBtnContainer = () => {
   });
 
   const prevPage = () => {
-    console.log('prev page');
+    let newPage = page - 1;
+    if (newPage < 1) {
+      newPage = numOfPages;
+    }
+    changePage(newPage);
   };
   const nextPage = () => {
-    console.log('next page');
+    let newPage = page + 1;
+    if (newPage > numOfPages) {
+      newPage = 1;
+    }
+    changePage(newPage);
   };
   return (
     <Wrapper>
