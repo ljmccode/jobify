dd1aff8fb9b8cba0fa60630b5d6488a7f6f9e0ed
Laura <lmcsay@gmail.com>
2022-07-24 19:25:09 -0500

    Update incorrect password error message

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -29,25 +29,29 @@ const register = async (req, res) => {
 
 const login = async (req, res) => {
   const { email, password } = req.body;
-  
+
   if (!email || !password) {
     throw new BadRequestError('Please provide email and password');
   }
-  const user = await User.findOne({ email }).select('+password')
+  const user = await User.findOne({ email }).select('+password');
   if (!user) {
     throw new UnauthenticatedError('Invalid Credentials');
   }
   const isPasswordCorrect = await user.comparePassword(password);
   if (!isPasswordCorrect) {
-    throw new UnauthenticatedError('Invalid Credentials: password');
+    throw new UnauthenticatedError('Invalid email or password');
   }
   const token = user.createJWT();
-  res.status(StatusCodes.OK).json({ user: {
-    email: user.email,
-    lastName: user.lastName,
+  res.status(StatusCodes.OK).json({
+    user: {
+      email: user.email,
+      lastName: user.lastName,
+      location: user.location,
+      name: user.name,
+    },
+    token,
     location: user.location,
-    name: user.name,
-  }, token, location: user.location });
+  });
 };
 
 const updateUser = async (req, res) => {
@@ -56,21 +60,21 @@ const updateUser = async (req, res) => {
     throw new BadRequestError('Please provide all values');
   }
 
-  const user = await User.findOne({ _id: req.user.userId })
+  const user = await User.findOne({ _id: req.user.userId });
 
-  user.email = email
-  user.name = name
-  user.lastName = lastName
-  user.location = location
+  user.email = email;
+  user.name = name;
+  user.lastName = lastName;
+  user.location = location;
 
-  await user.save()
+  await user.save();
 
   const token = user.createJWT();
   res.status(StatusCodes.OK).json({
     user,
     token,
     location: user.location,
-  })
+  });
 };
 
 export { register, login, updateUser };
